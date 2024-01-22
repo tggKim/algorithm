2b70be02ea816326f7112f9d17cb385937c1cd68
tggKim <138564756+tggKim@users.noreply.github.com>
2024-01-22 21:47:58 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 148 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        System.out.println(K/M+" "+K%M);
+    }
+}
