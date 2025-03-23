6ffb5857efb478629b6918f59cb35b4d70affc0c
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-03-24 02:58:33 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10892 ms, Memory: 312972 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,68 +1,59 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static ArrayList<Integer>[] arr;
-
+public class Main{
+    
+    static ArrayList<Integer>[] vec;
+    static int[] answer;
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int max = 0;
-
-        arr = new ArrayList[n + 1];
-        int[] arr1 = new int[n + 1];
-
-
-        // 배열 생성
-        for (int i = 1; i <= n; i++) {
-            arr[i] = new ArrayList<>();
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        vec = new ArrayList[N+1];
+        for(int i=1;i<=N;i++){
+            vec[i] = new ArrayList<>();
         }
-
-        // 배열 입력
-        for (int i = 0; i < m; i++){
+        answer = new int[N+1];
+        
+        for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            arr[b].add(a);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            vec[y].add(x);
         }
-
-        for (int i = 1; i <= n; i++){
-
-            int cnt = 1;
-            boolean[]  visited = new boolean[n + 1];
+        
+        int max = Integer.MIN_VALUE;
+        for(int i=1;i<=N;i++){
+            int count=0;
+            boolean[] visited = new boolean[N+1];
             visited[i] = true;
-
-            Queue<Integer> arr2 = new LinkedList<>();
-            arr2.add(i);
-
-            while (!arr2.isEmpty()){
-                int node = arr2.poll(); // Arraylist-> Stack으로 변경. O(N) -> O(1)
-
-                for (int integer : arr[node]){
-                    if (!visited[integer]) {
-                        visited[integer] = true;
-                        cnt += 1;
-                        arr2.add(integer);
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            while(!q.isEmpty()){
+                int pollNumber = q.poll();
+                for(int node : vec[pollNumber]){
+                    if(!visited[node]){
+                        visited[node] = true;
+                        count++;
+                        q.add(node);
                     }
                 }
             }
-
-            arr1[i] = cnt;
-            if (cnt > max){
-                max = cnt;
-            }
+            
+            answer[i]=count;
+            max = Math.max(max, count);
         }
 
-        for (int i = 1; i <= n; i++)
-            if(arr1[i] == max) {
-                sb.append(i).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=N;i++){
+            if(answer[i] == max){
+                sb.append(i+" ");
             }
-        System.out.print(sb.toString());
-
-
+        }
+        System.out.println(sb);
     }
 }
