eee1bc4acbe3ff83e99539c5a51ea07b940a52f4
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-04-30 09:57:44 +0900

    [Gold IV] Title: 최단경로, Time: 668 ms, Memory: 110572 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,58 +1,66 @@
 import java.util.*;
 import java.io.*;
+
 public class Main{
-    public static void main(String[] args)throws IOException{
+    
+    static ArrayList<Node>[] arr;
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(br.readLine());
-
-        ArrayList<ArrayList<Node>> arr = new ArrayList<>();
+        
+        arr = new ArrayList[V+1];
         boolean[] visited = new boolean[V+1];
         int[] result = new int[V+1];
         for(int i=0;i<=V;i++){
-            result[i]=Integer.MAX_VALUE;
-            arr.add(new ArrayList<Node>());
+            arr[i] = new ArrayList<>();
+            result[i] = Integer.MAX_VALUE;
         }
-
+        
         for(int i=0;i<E;i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            arr.get(u).add(new Node(v,w));
+            arr[u].add(new Node(v, w));
         }
-        result[K]=0;
+        
         PriorityQueue<Node> q = new PriorityQueue<>((a,b)->a.weight-b.weight);
+        result[K]=0;
         q.add(new Node(K,0));
         while(!q.isEmpty()){
             Node now = q.poll();
             visited[now.end]=true;
-            for(Node next : arr.get(now.end)){
-                if(!visited[next.end] && now.weight+next.weight<result[next.end]){
-                    result[next.end]=now.weight+next.weight;
-                    q.add(new Node(next.end,result[next.end]));
+            for(Node next : arr[now.end]){
+                if(!visited[next.end] && now.weight + next.weight < result[next.end]){
+                    result[next.end] = now.weight + next.weight;
+                    q.add(new Node(next.end, result[next.end]));
                 }
             }
         }
+        
+        StringBuilder sb = new StringBuilder();
         for(int i=1;i<=V;i++){
             if(result[i]==Integer.MAX_VALUE){
-                bw.write("INF\n");
+                sb.append("INF\n");
             }
             else{
-                bw.write(result[i]+"\n");
+                sb.append(result[i]+"\n");
             }
         }
-        bw.flush();
+        System.out.println(sb);
     }
+    
     static class Node{
-        int end;
         int weight;
-        public Node(int end,int weight){
-            this.end=end;
-            this.weight=weight;
+        int end;
+        public Node(int end, int weight){
+            this.end = end;
+            this.weight = weight;
         }
     }
 }
