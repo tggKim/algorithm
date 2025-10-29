a9efcf66b50e9d58baf5354699b545fde5dffef2
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-10-29 23:15:07 +0900

    [Gold IV] Title: 최단경로, Time: 712 ms, Memory: 120148 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,65 +1,74 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+public class Main {
     
-    static ArrayList<Node>[] arr;
+    static List<Node>[] list;
+    static boolean[] visited;
+    static int[] answer;
     
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
-        
-        arr = new ArrayList[V+1];
-        boolean[] visited = new boolean[V+1];
-        int[] result = new int[V+1];
-        for(int i=0;i<=V;i++){
-            arr[i] = new ArrayList<>();
-            result[i] = Integer.MAX_VALUE;
+        int K = Integer.parseInt(br.readLine());        
+
+        list = new ArrayList[V+1];
+        visited = new boolean[V+1];
+        answer = new int[V+1];
+        for(int i=0;i<=V;i++) {
+            list[i] = new ArrayList<>();
+            answer[i] = Integer.MAX_VALUE;
         }
         
-        for(int i=0;i<E;i++){
+        for(int i=0;i<E;i++) {
             st = new StringTokenizer(br.readLine());
+            
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            arr[u].add(new Node(v, w));
+            
+            list[u].add(new Node(v, w));
         }
         
-        PriorityQueue<Node> q = new PriorityQueue<>((a,b)->a.weight-b.weight);
-        result[K]=0;
-        q.add(new Node(K,0));
-        while(!q.isEmpty()){
+        PriorityQueue<Node> q = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        q.add(new Node(K, 0));
+        answer[K] = 0;
+        
+        while(!q.isEmpty()) {
             Node now = q.poll();
-            visited[now.end]=true;
-            for(Node next : arr[now.end]){
-                if(!visited[next.end] && now.weight + next.weight < result[next.end]){
-                    result[next.end] = now.weight + next.weight;
-                    q.add(new Node(next.end, result[next.end]));
+            
+            if(visited[now.point]){
+                continue;
+            }
+            visited[now.point] = true;
+            
+            for(Node next : list[now.point]) {
+                if(answer[next.point] > answer[now.point] + next.weight) {
+                    answer[next.point] = answer[now.point] + next.weight;
+                    q.add(new Node(next.point, answer[next.point]));
                 }
             }
+         
         }
         
+        
         StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=V;i++){
-            if(result[i]==Integer.MAX_VALUE){
-                sb.append("INF\n");
-            }
-            else{
-                sb.append(result[i]+"\n");
-            }
+        for (int i = 1; i <= V; i++) {
+            if (answer[i] == Integer.MAX_VALUE) sb.append("INF\n");
+            else sb.append(answer[i]).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
     
-    static class Node{
+    static class Node {
+        int point;
         int weight;
-        int end;
-        public Node(int end, int weight){
-            this.end = end;
+        
+        Node(int point, int weight) {
+            this.point = point;
             this.weight = weight;
         }
     }
