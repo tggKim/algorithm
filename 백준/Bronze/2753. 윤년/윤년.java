0e06934c2ad0f88eebc895e08accf75cce539754
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-11-02 23:04:54 +0900

    [Bronze V] Title: 윤년, Time: 176 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int year = scanner.nextInt();
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 ==0) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
