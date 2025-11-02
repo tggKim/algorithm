15780445d10b6cb7be41bdf41d5c713160ee2570
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-11-02 22:58:49 +0900

    [Bronze IV] Title: 세수정렬, Time: 184 ms, Memory: 17876 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,16 +1,16 @@
 import java.util.*;
-import java.io.*;
-public class Main{
-    public static void main(String[] args)throws IOException{
-        ArrayList<Integer> list = new ArrayList();
+
+public class Main {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        StringTokenizer st = new StringTokenizer(s.nextLine());
-        for(int i=0;i<3;i++){
-            list.add(Integer.parseInt(st.nextToken()));
-        }
-        Collections.sort(list);
-        for(int i : list){
-            System.out.print(i+" ");
-        }
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int C = s.nextInt();
+        
+        List<Integer> list = List.of(A, B, C);
+        
+        list.stream()
+            .sorted((a, b) -> a - b)
+            .forEach(number -> System.out.print(number + " "));
     }
 }
