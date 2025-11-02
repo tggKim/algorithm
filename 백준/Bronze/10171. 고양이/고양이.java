0b03ce865ea5c29f4e1de444f49ca8cb96bf991b
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-11-02 22:39:37 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("\\    /\\");
+        sb.append("\n )  ( \')");
+        sb.append("\n(  /  )");
+        sb.append("\n \\(__)|");
+        
+        System.out.print(sb.toString());
+    }
+}
