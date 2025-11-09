6226ae4165e106159707355924ff24d8aece6d2d
kimminjae <138564756+tggKim@users.noreply.github.com>
2025-11-09 16:29:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int N = s.nextInt();
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        
+        System.out.print(sb.toString());
+    }
+}
