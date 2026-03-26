02fc9970008a8636e8d5fddedb0cd67ec8f8befb
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-03-26 10:01:31 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 21832 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.kt
+++ 백준/Bronze/1546. 평균/평균.kt
@@ -3,14 +3,14 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val N = br.readLine().toInt()
 
-    val list = br.readLine().split(" ")
+    val list = br.readLine().split(" ").map { it.toDouble() }
 
     var sum = 0.0
     var max = 0.0
     for(i in list) {
-        sum += i.toDouble();
-        max = maxOf(max, i.toDouble())
+        sum += i
+        max = maxOf(max, i)
     }
 
-    println((sum / max * 100.0)/N);
+    println((sum / max * 100)/N)
 }
