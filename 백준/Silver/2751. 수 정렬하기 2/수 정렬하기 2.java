cecaf956e70727f1c30a7ffe844077336b2eb457
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-04-02 14:55:02 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1208 ms, Memory: 142992 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -6,20 +6,20 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
 		
+		int[] arr = new int[N];
 		for(int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
 		sort(arr, 0, arr.length - 1);
-		
-		for(int i = 0; i < arr.length; i++) {
-			bw.write(arr[i] + "\n");
-		}
-		
-		bw.flush();
-		bw.close();
+
+        for(int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
 	}
 	
 	static void sort(int[] arr, int start, int end) {
@@ -33,17 +33,19 @@ public class Main {
 	
 	static int partition(int[] arr, int start, int end) {
 		Random random = new Random();
-		int num = start + random.nextInt(end - start + 1);
-		int temp = arr[end];
-		arr[end] = arr[num];
-		arr[num] = temp;
+		int randomNum = start + random.nextInt(end - start + 1);
+		
+		int temp = arr[randomNum];
+		arr[randomNum] = arr[end];
+		arr[end] = temp;
 		
 		int index = start;
+		
 		for(int i = start; i < end; i++) {
 			if(arr[i] < arr[end]) {
-				temp = arr[i];
-				arr[i] = arr[index];
-				arr[index] = temp;
+				temp = arr[index];
+				arr[index] = arr[i];
+				arr[i] = temp;
 				
 				index++;
 			}
