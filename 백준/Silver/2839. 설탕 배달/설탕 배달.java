698111c50c5554c0630a6c6afe3807607f1ddc67
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-04-07 11:56:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,31 @@
-import java.io.*;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        int count = 0; // 봉지 개수
-        
-        // 설탕이 남아있는 동안 계속 반복
-        while (N >= 0) {
-            // 1. 남은 설탕이 5로 딱 나누어 떨어지면 개이득!
-            if (N % 5 == 0) {
-                count += (N / 5);   // 5kg 봉지 개수만큼 더해주고
-                System.out.println(count);
-                return;             // 정답을 찾았으니 프로그램 바로 종료
-            }
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int N = s.nextInt();
+		
+		if(N % 5 == 0) {
+			System.out.println(N / 5);
+			return;
+		} 
+		
+		int count = 0;
+		while(true) {
+			if(N % 5 == 0) {
+				break;
+			}
             
-            // 2. 5로 안 나누어 떨어지면, 3kg 봉지 하나를 쓴다.
-            N -= 3;
-            count++;
-        }
-        
-        // 3. 3kg씩 계속 뺐는데 결국 N이 음수(-1, -2)가 되어버렸다면?
-        // 이건 3과 5로 딱 맞출 수 없는 무게라는 뜻 (예: 4kg)
-        System.out.println(-1);
-    }
+			N -= 3;
+			if(N < 0) {
+				System.out.println(-1);
+				return;
+			}
+			count++;
+		}
+		
+		count += (N / 5);
+		
+		System.out.println(count);
+	}
 }
