30b15a6897a53196cd58045ffba249472df65aca
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-04-08 10:13:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 848 ms, Memory: 207648 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,29 +5,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		Map<Integer, Integer> map = new HashMap<>();
+		int[] arr = new int[20000001];
 		
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			Integer num = Integer.parseInt(st.nextToken());
-			if(map.containsKey(num)) {
-				map.put(num, map.get(num) + 1);
-			} else {
-				map.put(num, 1);
-			}
+			arr[num + 10000000]++;
 		}
 		
-		int sum = 0;
 		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < M; i++) {
 			Integer num = Integer.parseInt(st.nextToken());
-			if(map.containsKey(num)) {
-				bw.write(map.get(num) + " ");
-			} else {
-				bw.write(0 + " ");
-			}
+			bw.write(arr[num + 10000000] + " ");
 		}
 		
 		bw.flush();
