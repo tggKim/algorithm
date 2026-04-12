e8e843514f86d6e5c80178fa1634e3e6bebd3372
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-04-13 00:35:26 +0900

    [Silver V] Title: 집합, Time: 1020 ms, Memory: 300060 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,38 +1,50 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<Integer> s = new HashSet<>();
         
         int M = Integer.parseInt(br.readLine());
-        int bitset = 0; // 비트마스킹을 위한 정수 (0이 공집합)
-        
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String op = st.nextToken();
-            
-            if (op.equals("all")) {
-                bitset = (1 << 21) - 1; // 1~20까지 모든 비트를 1로 켬
-            } else if (op.equals("empty")) {
-                bitset = 0; // 모든 비트를 0으로 끔
-            } else {
-                int num = Integer.parseInt(st.nextToken());
-                
-                if (op.equals("add")) {
-                    bitset |= (1 << num); // num번째 비트를 1로 (OR)
-                } else if (op.equals("remove")) {
-                    bitset &= ~(1 << num); // num번째 비트를 0으로 (AND NOT)
-                } else if (op.equals("check")) {
-                    sb.append((bitset & (1 << num)) != 0 ? 1 : 0).append("\n");
-                } else if (op.equals("toggle")) {
-                    bitset ^= (1 << num); // num번째 비트 상태 반전 (XOR)
+            String str = st.nextToken();
+            int number = 0;
+            if(!str.equals("all") && !str.equals("empty")) {
+                 number = Integer.parseInt(st.nextToken());
+            }
+
+            if(str.equals("add")) {
+                s.add(number);
+            } else if(str.equals("remove")) {
+                s.remove(number);
+            } else if(str.equals("check")) {
+                if(s.contains(number)) {
+                    bw.write("1\n");
+                } else {
+                    bw.write("0\n");
+                }
+            } else if(str.equals("toggle")) {
+                if(s.contains(number)) {
+                    s.remove(number);
+                } else {
+                    s.add(number);
+                }
+            } else if(str.equals("all")) {
+                for(int j = 1; j <= 20; j++) {
+                    s.add(j);
+                }
+            } else if(str.equals("empty")) {
+                for(int j = 1; j <= 20; j++) {
+                    s.remove(j);
                 }
             }
         }
-        System.out.print(sb.toString()); // 한 번에 출력
+
+        bw.flush();
+        bw.close();
     }
 }
