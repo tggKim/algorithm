bd3346b7162b70bd1dd6228aaef18d6ad747bb52
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-04-13 09:41:06 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 26188 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -3,46 +3,35 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-    	Set<String> set0 = new HashSet<>();
-		Set<String> set1 = new HashSet<>();
-		Set<String> set2 = new HashSet<>();
+		List<String> list = new ArrayList<>();
+		Set<String> set = new HashSet<>();
 		
 		for(int i = 0; i < N; i++) {
 			String str = br.readLine();
-			set0.add(str);
-			set1.add(str);
+			set.add(str);
 		}
 		
-		for(int i = 0; i < M; i++) {
+		for(int i = 0; i < N; i++) {
 			String str = br.readLine();
-			set0.add(str);
-			set2.add(str);
-		}
-
-        List<String> list = new ArrayList<>(set0);
-        List<String> answerList = new ArrayList<>();
-		Collections.sort(list);
-		for(int i = 0; i < list.size(); i++) {
-			String str = list.get(i);
-			
-			if(set1.contains(str) && set2.contains(str)) {
-    			answerList.add(str);
+			if(set.contains(str)) {
+				list.add(str);
 			}
 		}
 		
-		bw.write(answerList.size() + "\n");
-        for(int i = 0; i < answerList.size(); i++) {
-            bw.write(answerList.get(i) + "\n");
-        }
-
-        bw.flush();
-        bw.close();
+		Collections.sort(list);
+		bw.write(list.size() + "\n");
+		for(int i = 0; i < list.size(); i++) {
+			bw.write(list.get(i) + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
 	}
 }
