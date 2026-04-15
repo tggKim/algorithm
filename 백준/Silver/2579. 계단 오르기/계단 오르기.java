49f0ab4901c1c890edb3adabbfddd2de3456d366
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-04-15 15:46:13 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,32 +1,46 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
+	static int[] dp;
+	static int[] arr; 
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N + 1];
-		int[] answer = new int[N + 1];
 		
-		for(int i = 1; i < N + 1; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
+		dp = new int[N + 1];
+		arr = new int[N + 1];
 
-        if(N == 1) {
-            System.out.println(arr[1]);
-            return;
-        } else if(N == 2) {
-            System.out.println(arr[1] + arr[2]);
-            return;
+        for(int i = 1; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
 		
-		answer[1] = arr[1];
-		answer[2] = arr[1] + arr[2];
-		
-		for(int i = 3; i < N + 1; i++) {
-			answer[i] = Math.max(answer[i - 2] + arr[i], answer[i-3] + arr[i-1] + arr[i]);
+		if(N == 1) {
+			System.out.println(arr[1]);
+			return;
+		} else if(N == 2) {
+			System.out.println(arr[1] + arr[2]);
+			return;
 		}
 		
-		System.out.println(answer[N]);
+		dp[1] = arr[1];
+		dp[2] = arr[1] + arr[2];
+		
+		System.out.println(dp(N));
+	}
+	
+	static int dp(int number) {
+		if(number == 0) {
+			return 0;
+		}
+		
+		if(dp[number] > 0) {
+			return dp[number];
+		}
+		
+		dp[number] = Math.max(dp(number - 2) + arr[number], dp(number - 3) + arr[number - 1] + arr[number]);
+		
+		return dp[number];
 	}
 }
