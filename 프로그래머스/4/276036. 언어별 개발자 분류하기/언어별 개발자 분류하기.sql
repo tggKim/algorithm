362318994bd6912179dacb22f99990d023f3771e
kimminjae <138564756+tggKim@users.noreply.github.com>
2026-08-27 17:00:30 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,38 +1,16 @@
-SELECT T1.GRADE, T1.ID, T1.EMAIL
-FROM (
-    SELECT
-        CASE
-            WHEN (
-                EXISTS (
-                    SELECT 1
-                    FROM SKILLCODES
-                    WHERE SKILL_CODE & CODE = CODE AND CATEGORY = 'Front End'
-                ) 
-                AND
-                EXISTS (
-                    SELECT 1
-                    FROM SKILLCODES
-                    WHERE SKILL_CODE & CODE = CODE AND NAME = 'Python'
-                )
-            )   THEN 'A'
-            WHEN (
-                EXISTS (
-                    SELECT 1
-                    FROM SKILLCODES
-                    WHERE SKILL_CODE & CODE = CODE AND NAME = 'C#'
-                )
-            )   THEN 'B'
-            WHEN (
-                EXISTS (
-                    SELECT 1
-                    FROM SKILLCODES
-                    WHERE SKILL_CODE & CODE = CODE AND CATEGORY = 'Front End'
-                )
-            )   THEN 'C'
-        END AS GRADE
-    ,   ID
-    ,   EMAIL
-    FROM DEVELOPERS
-    ORDER BY GRADE, ID
-) T1
-WHERE T1.GRADE IS NOT NULL
+SELECT
+    CASE
+        WHEN MAX(T2.CATEGORY = 'Front End') AND MAX(T2.NAME = 'Python') THEN 'A'
+        WHEN MAX(T2.NAME = 'C#') THEN 'B'
+        WHEN MAX(T2.CATEGORY = 'Front End') THEN 'C'
+    END AS GRADE
+,   T1.ID
+,   T1.EMAIL
+FROM DEVELOPERS T1
+INNER JOIN SKILLCODES T2
+    ON T1.SKILL_CODE & T2.CODE != 0 AND (
+        T2.CATEGORY = 'Front End' OR T2.NAME = 'Python' OR T2.NAME = 'C#'
+    )
+GROUP BY T1.ID, T1.EMAIL
+HAVING GRADE IS NOT NULL
+ORDER BY GRADE, T1.ID
